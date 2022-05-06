991de441779e002c31e07fb7207bdb55dc21d991
adamaru <adamaru8700@icloud.com>
2022-05-06 00:41:38 +0000

    3章3.11.2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
   end
   
   def create
-    params[:content]
     @post = Post.new(content: params[:content])
     @post.save
     redirect_to posts_index_url #コントローラー内で名前付きルーティングを記述場合、…_urlと記述
@@ -12,6 +11,7 @@ class PostsController < ApplicationController
   
   def index
     @posts = Post.all
+    
   end
   
   def show
