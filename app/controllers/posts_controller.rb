39fcbcee87225286b06456501975183cb17de1f3
adamaru <adamaru8700@icloud.com>
2022-05-07 11:17:12 +0000

    5章5.3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,8 +25,11 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to posts_index_url
+    if @post.save
+      redirect_to posts_index_url
+    else
+      redirect_to edit_post_url @post  # (@post.id)を　@post　とシンプル記述可能
+    end
   end
   
   def destroy
