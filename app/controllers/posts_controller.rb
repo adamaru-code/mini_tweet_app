5dc92994b2be991806550902e272325895e20955
adamaru <adamaru8700@icloud.com>
2022-05-07 12:34:46 +0000

    5章終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,17 @@
 class PostsController < ApplicationController
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to posts_index_url #コントローラー内で名前付きルーティングを記述場合、…_urlと記述
+    if @post.save
+      flash[:notice] = "投稿しました！"
+      redirect_to posts_index_url #コントローラー内で名前付きルーティングを記述場合、…_urlと記述
+    else
+      render :new
+    end
   end
   
   def index
@@ -37,6 +42,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました。"
     redirect_to posts_index_url
   end
 end
